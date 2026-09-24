3296375e7812663e91d047d325defb0b4fa08a53
hndrwn-dk <hendrawandaryono.karso@gmail.com>
2026-09-24 17:47:25 +0800

    chore: bump version to 1.0.7+48

--- lib/core/constants/app_version.dart
+++ lib/core/constants/app_version.dart
@@ -1,7 +1,7 @@
 /// Application version info. Keep in sync with pubspec.yaml version.
 class AppVersion {
-  static const String version = '1.0.6';
-  static const int buildNumber = 47;
+  static const String version = '1.0.7';
+  static const int buildNumber = 48;
 
   /// Full display string, e.g. "1.0.1 (18)"
   static String get display => '$version ($buildNumber)';
